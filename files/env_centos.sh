cc44eef2729b86e8ddafb5c997f0f517bd2074e2
James Zhang <zhangxinjia@huawei.com>
2018-02-02 16:53:43 +0800

    make environment variables work!

--- files/env_centos.sh
+++ files/env_centos.sh
@@ -1,4 +1,10 @@
-export JAVA_HOME=/usr/estuary/packages/openjdk-1.8.0/jdk8u-server-release-1609
+#!/bin/sh
+
+
+echo "export JAVA_HOME=/usr/estuary/packages/openjdk-1.8.0/jdk8u-server-release-1609" >> ~/.bashrc
 #export JAVA_HOME=/usr/lib/jdk/ezdk-1.8.0_131-8.21.0.54-eval-linux_aarch64
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=$JAVA_HOME/lib
+echo  "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
+echo "export CLASSPATH=\$JAVA_HOME/lib" >> ~/.bashrc
+
+sleep 1
+source ~/.bashrc
--- files/env_ubuntu.sh
+++ files/env_ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=$JAVA_HOME/lib
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64" >> ~/.bashrc
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
+echo "export CLASSPATH=\$JAVA_HOME/lib" >> ~/.bashrc
+
+sleep 1
+source ~/.bashrc
