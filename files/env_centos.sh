31e73220a598c8ad5fa5cd868e0ec07d31de43ca
James Zhang <zhangxinjia@huawei.com>
2018-02-02 16:14:57 +0800

    Optimize, specjvm thirdly.

---
+++ files/env_centos.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/estuary/packages/openjdk-1.8.0/jdk8u-server-release-1609
+#export JAVA_HOME=/usr/lib/jdk/ezdk-1.8.0_131-8.21.0.54-eval-linux_aarch64
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=$JAVA_HOME/lib
---
+++ files/env_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=$JAVA_HOME/lib
